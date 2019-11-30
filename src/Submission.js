3f140d998d7b6e4f25ce9e20c207dfddfa3f134d
Nikko Angelo Dumrique <dumriquenikko@gmail.com>
2019-11-30 15:33:47 -0800

    merged my files from the old repo

--- src/Submission.js
+++ src/Submission.js
@@ -1,27 +1,22 @@
-
-
-
 // The purpose of this script is to incorporate the submission system
 
 // #region Class
 class problemArea{
-    constructor(summary, priority, location) {
+    constructor(summary, priority, location, idNum) {
         this.summary = summary;
         this.priority = level;
         this.location = location;
-        this.id
+        this.id = idNum;
     }
 }
 // #endregion
 
-
-
 // #region Functions
 
 //purpose: 
-//function is to consume input information
-function submitPA(summary,priority, location) {
-    return(problemArea(summary, priority, location))
+//function is to consume input information about the issue and create
+function submitPA(summary,priority, location, idNum) {
+    problemArea(summary, priority, location, idNum);
 }
 // #endregion 
 
