97f8ccbf20a46b9aeec3a7aa5881dff1735ff7be
Nikko Angelo Dumrique <dumriquenikko@gmail.com>
2019-11-30 14:39:00 -0800

    Created a submission script

---
+++ Submission.js
@@ -0,0 +1,30 @@
+
+
+
+// The purpose of this script is to incorporate the submission system
+
+// #region Class
+class problemArea{
+    constructor(summary, priority, location) {
+        this.summary = summary;
+        this.priority = level;
+        this.location = location;
+        this.id
+    }
+}
+// #endregion
+
+
+
+// #region Functions
+
+//purpose: 
+//function is to consume input information
+function submitPA(summary,priority, location) {
+    return(problemArea(summary, priority, location))
+}
+// #endregion 
+
+
+
+
